a11235614e85984ae40349e0b055cc693fe1b786
carlosvldz <me@carlosvldz.com>
2020-10-21 20:00:22 -0500

    add env.port

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import app from './app';
 import '@babel/polyfill';
+import config from './config';
 import 'regenerator-runtime';
 
 async function main() {
-    await app.listen(3000);
+    await app.listen(config.PORT, '0.0.0.0');
     console.log('Server on port 3000')
 };
 
